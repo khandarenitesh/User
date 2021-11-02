e21676655401436c91de0dcceba85c465f9fb12a
Nitesh Khandare <55987465+khandarenitesh@users.noreply.github.com>
2021-11-02 21:38:10 +0530

    Create user.module.ts

---
+++ user.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { ProfileComponent } from "./profile.component";
+import {userRoutes } from "./users.routes";
+import { LoginComponent } from "./login.component";
+import { FormsModule } from "@angular/forms";
+
+@NgModule ({
+    imports:[
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild(userRoutes)
+
+    ],
+    declarations:[
+        ProfileComponent,
+        LoginComponent
+    ],
+    providers:[],
+
+})
+export class UserModule{
+
+}
